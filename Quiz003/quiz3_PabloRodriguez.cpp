44563a98cd39a3a42f839cbc1bba35fcbcb40b49
JOSEpablo2 <josepablorodriguezrubi663@gmail.com>
2026-04-22 14:30:36 -0600

    Añadiendo quiz3
    Signed-off-by: JOSEpablo2 <josepablorodriguezrubi663@gmail.com>

---
+++ Quiz003/quiz3_PabloRodriguez.cpp
@@ -0,0 +1,49 @@
+//Ejercicio:Vagones del tren
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+bool cargar(int inicio, int p, int v) {
+    for (int i = 0; i <= p; i++) {
+            return inicio += v;
+            
+        }
+    }
+         
+         
+            
+        
+    
+        
+bool consultar(int inicio, int C, int p, int v) {
+    for (int i = p; i <= p; i++) {
+        return v;
+
+    }
+}
+
+
+
+int main() {
+    
+    int inicio = 5;
+    
+    int p;
+    cout << "Ingrese hasta que vagon desea agregar carga: ";
+    cin >> p;
+    int v;
+    cout << "Cuantas unidades de carga quiere agregar: ";
+    cin >> v;
+
+    int C;
+    cout << "Cual vagon desea saber la carga: ";
+    cin >> C;
+
+    cargar(inicio, p, v);
+
+    
+    
+return 0;
+
+}
